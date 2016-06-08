076b883ebfb3888327cbaf2b1d51532a6373699b
Pablo Schandin <pablo.schandin@avature.net>
2016-06-08 01:34:24 -0300

    Added main search functionality

--- main.py
+++ main.py
@@ -1,147 +1,65 @@
+#!/usr/bin/python
+
 from gi.repository import Gtk
-import sys
-import webbrowser
-import json
-from addmenu import AddMenuWindow
-from deletemenu import DeleteMenuWindow
-from listmenu import ListMenuWindow
+from windows import Window, SecondWindows
+from menubar import MenuBar
 from classes import StuffDoer
+import json
 
-class Searchapp():
+class App():
 
-	def __init__(self):
+    def __init__(self):
 
-		""" The constructor has all the windows' generators and menus. """
+        self.app = Window()
+        self.app.set_size(400,70)
+        self.app.set_title("Searchapp")
 
-		win = Gtk.Window() # Generates empty window
-		win.set_title("Searchapp") # Sets title
-		win.set_default_size(400,70) # Sets size (x,y)
-		win.set_position(Gtk.WindowPosition.CENTER) # Sets position
-		win.connect("destroy", self.closeWin) # Close button action
+        self.menuu = MenuBar()
+        self.vbox = Gtk.VBox(False, 10)
+        self.vbox.pack_start(self.menuu.return_menu(), False, False, 0)
 
-		menu = Gtk.MenuBar() #Generates empty menu bar
+        self.textentry = Gtk.Entry()
+        self.textentry.connect("activate", self.do_stuff)
 
-		filemenu = Gtk.Menu() # Generates button on menu
-		filem = Gtk.MenuItem("File") # Create name for button on menu
-		filem.set_submenu(filemenu) # Attach "File" to button
+        self.table = Gtk.Table(2, 1, False)
+        self.app.add_to_win(self.table)
+        self.table.attach(self.vbox, 0, 1, 0, 1)
+        self.table.attach(self.textentry, 0, 1, 1, 2, xpadding=10, ypadding=10)
 
-		add = Gtk.MenuItem("Add") # Creates submenu
-		add.connect("activate", self.add_menu) # Creates close action
-		filemenu.append(add) # Append
+        accGroup = Gtk.AccelGroup()
+        key, modifier = Gtk.accelerator_parse('Escape')
+        accGroup.connect(key, modifier, Gtk.AccelFlags.VISIBLE, Gtk.main_quit)
+        self.app.add_accel_group(accGroup)
 
-		delete = Gtk.MenuItem("Delete")
-		delete.connect("activate", self.delete_menu)
-		filemenu.append(delete)
+        self.app.show_all()
 
-		lst = Gtk.MenuItem("List")
-		lst.connect("activate", self.list_menu)
-		filemenu.append(lst)
+    def do_stuff(self, widget):
 
-		exit = Gtk.MenuItem("Exit") # Creates submenu
-		exit.connect("activate", Gtk.main_quit) # Creates close action
-		filemenu.append(exit) # Append
+        text = widget.get_text() # Gets text from widget
+        parsed = text.split(" ", 1) # Splits until the first space
 
+        with open('config.json', 'rb') as json_file:
+            data = json.load(json_file)
+            alldata = data['all']
+            commandsdata = data['commands']
+            if parsed[0] == '!default' and parsed[1] == 'none':
+                StuffDoer().set_default_none()
+            if parsed[0] == '!default' and parsed[1] != 'none':
+                StuffDoer().set_default(parsed[1])
+            if commandsdata[0]['val'] == 'true':
+                StuffDoer().search_default(text)
+            if commandsdata[0]['val'] == 'false' and len(parsed) > 1:
+                StuffDoer().search_common(parsed[0], parsed[1])
+            else:
+                dialog2 = Gtk.MessageDialog(None, 0, Gtk.MessageType.INFO,Gtk.ButtonsType.OK, "Error")
+                dialog2.format_secondary_text("Doesn't work that way. See 'How to' on the Menu.")
+                dialog2.run()
+                dialog2.destroy()
 
-		helpmenu = Gtk.Menu()
-		helpm = Gtk.MenuItem("Help")
-		helpm.set_submenu(helpmenu)
+# Main loop
 
-		howto = Gtk.MenuItem("How to")
-		howto.connect("activate", self.how_to_menu)
-		helpmenu.append(howto)
-
-		about = Gtk.MenuItem("About")
-		about.connect("activate", self.about_menu)
-		helpmenu.append(about)
-
-		menu.append(filem) # Append the menus to the menu bar
-		menu.append(helpm)
-
-		vbox = Gtk.VBox(False, 10) # Orginize child widgets into a column (homogeneous,spacing between widgets)
-		vbox.pack_start(menu, False, False, 0) # pack at top (child, expand, fill, padding)
-
-		textentry = Gtk.Entry() # Create text box
-		textentry.connect("activate", self.do_stuff) # Create action
-
-		table = Gtk.Table(2, 1, False) # Create table to locate the widgets (rows, cols, homogeneous)
-		win.add(table)
-
-		table.attach(vbox,0,1,0,1) # Attach to talbe (widget, from x, to x, from y, to y)
-		table.attach(textentry,0,1,1,2, xpadding=10,ypadding=10) # (xpadding, ypadding)
-
-		win.show_all() # Display Window
-
-	def closeWin(self, win):
-
-		"""Function to close main window."""
-
-		Gtk.main_quit()
-
-	def how_to_menu(self, widget):
-
-		"""Generates a dialog with the 'How to' instructions."""
-
-		dialog = Gtk.MessageDialog(None, 0, Gtk.MessageType.INFO,Gtk.ButtonsType.OK, "How to use Searchapp")
-		dialog.format_secondary_text("g = Google    y = Youtube    w = Wikipedia")
-		dialog.run()
-
-		dialog.destroy()
-
-	def about_menu(self, widget):
-
-		"""Generates an About Dialog. Can uncomment 'website'. Trying to fix the logo."""
-
-		about = Gtk.AboutDialog()
-		about.set_program_name("Searchapp")
-		about.set_version("1.2")
-		about.set_copyright("(c) 2015, RQ23A")
-		about.set_comments("A quicker way to find your stuff.")
-		about.set_logo_icon_name(None)
-#		about.set_website("")
-#		about.set_logo_icon_name(Gtk.gdk.pixbuf_new_from_file("search.png"))
-		about.run()
-		about.destroy()
-
-	def add_menu(self, window):
-
-		sub = AddMenuWindow()
-
-	def delete_menu(self, window):
-
-		delete = DeleteMenuWindow()
-
-	def list_menu(self, window):
-
-		listmenu = ListMenuWindow()
-
-	def do_stuff(self, widget):
-
-		"""This function does the searches and pops up a windows for incorrect input."""
-
-		text = widget.get_text() # Gets text from widget
-		parsed = text.split(" ", 1) # Splits until the first space
-
-		with open('config.json', 'rb') as json_file:
-			data = json.load(json_file)
-			alldata = data['all']
-			commandsdata = data['commands']
-			if parsed[0] == '!default' and parsed[1] == 'none':
-				StuffDoer().set_default_none()
-			if parsed[0] == '!default' and parsed[1] != 'none':
-				StuffDoer().set_default(parsed[1])
-			if commandsdata[0]['val'] == 'true':
-				StuffDoer().search_default(text)
-			if commandsdata[0]['val'] == 'false' and len(parsed) > 1:
-				StuffDoer().search_common(parsed[0], parsed[1])
-			else:
-				dialog2 = Gtk.MessageDialog(None, 0, Gtk.MessageType.INFO,Gtk.ButtonsType.OK, "Error")
-				dialog2.format_secondary_text("Doesn't work that way. See 'How to' on the Menu.")
-				dialog2.run()
-				dialog2.destroy()
-
-# Main loop.
 def main():
-	app = Searchapp()
-	Gtk.main()
-if __name__ == "__main__":
-	main()
+    app = App()
+    Gtk.main()
+if __name__=="__main__":
+    main()
